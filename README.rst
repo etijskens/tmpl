95923fa9142e9ab11810253ab1a762912fd9e7a4
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2021-07-28 14:44:46 +0200

    "And so this begun..."

---
+++ README.rst
@@ -0,0 +1,17 @@
+====
+tmpl
+====
+
+
+
+<Enter a one-sentence description of this project here.>
+
+
+* Free software: MIT license
+* Documentation: https://tmpl.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
---
+++ tmpl/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package tmpl
+=======================================
+
+Top-level package for tmpl.
+"""
+
+__version__ = "0.0.0"
+
+
+def hello(who='world'):
+    """'Hello world' method.
+
+    :param str who: whom to say hello to
+    :returns: a string
+    """
+    result = "Hello " + who
+    return result
+
+# Your code here...
